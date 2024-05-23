53509ba8d2bef71f9cefd9ecb68547ddff36fd0a
garlic0x1 <garlic1@protonmail.com>
2024-05-23 14:51:14 -0500

    adsf

--- treesitter.lisp
+++ treesitter.lisp
@@ -281,3 +281,43 @@
 
 (defcfun "ts_language_delete" :void
   (language :pointer))
+
+(defcfun "ts_language_symbol_count" :uint32
+  (language :pointer))
+
+(defcfun "ts_language_state_count" :uint32
+  (language :pointer))
+
+(defcfun "ts_language_symbol_name" :string
+  (language :pointer)
+  (symbol ts-symbol))
+
+(defcfun "ts_language_symbol_for_name" ts-symbol
+  (language :pointer)
+  (string :string)
+  (length :uint32)
+  (named-p :bool))
+
+(defcfun "ts_language_field_count" :uint32
+  (language :pointer))
+
+(defcfun "ts_language_field_name_for_id" :string
+  (language :pointer)
+  (id ts-field-id))
+
+(defcfun "ts_language_field_id_for_name" ts-field-id
+  (language :pointer)
+  (name :string)
+  (length :uint32))
+
+(defcfun "ts_language_symbol_type" ts-symbol-type
+  (language :pointer)
+  (symbol ts-symbol))
+
+(defcfun "ts_language_version" :uint32
+  (language :pointer))
+
+(defcfun "ts_language_next_state" ts-state-id
+  (language :pointer)
+  (state ts-state-id)
+  (symbol ts-symbol))
